0ad46298b7927667951998ed3b188342d50e06ea
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-02-28 17:25:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 exports.min = function min (array) {
   if( array === undefined || array.length < 1) return 0;
-  return Math.min.apply(array);
+  return Math.min.(...array);
 }
 
 exports.max = function max (array) {
   if( array === undefined || array.length < 1) return 0;
-  return Math.max.apply(array);
+  return Math.max.(...array);
 }
 
 exports.avg = function avg (array) {
   if( array === undefined || array.length < 1) return 0;
-  return array.reduce((accumulator, sum) => accumulator + sum)/array.length;
+  return array.reduce((accumulator, sum) => accumulator + sum, 0)/array.length;
 }
